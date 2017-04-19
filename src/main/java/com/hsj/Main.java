5e786b7871935744f38f80435d2d7d798df07189
sjhuangx <sjhuangx@gmail.com>
2017-04-19 22:20:26 +0800

    initial maven project structure and add note folder.

---
+++ src/main/java/com/hsj/Main.java
@@ -0,0 +1,12 @@
+package com.hsj;
+
+/**
+ * @author Allen on 2017/4/19
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main Class");
+    }
+
+}
